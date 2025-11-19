67e9a7f20949a9af0dd390f5ffdcf8df3b0e9ce9
Gustavo Henricchi <gustavohenricchikrul@gmail.com>
2025-11-19 09:43:44 -0300

    Delete calculadora directory

--- calculadora/detectarEntrada.js
+++
@@ -1,8 +0,0 @@
-function detectarEntrada(valor) {
-    if (!isNaN(valor)) return "número";
-    else if (/^[a-zA-Z]+$/.test(valor)) return "letra";
-    else return "caractere especial";
-  }
-  
-  module.exports = detectarEntrada;
-  
